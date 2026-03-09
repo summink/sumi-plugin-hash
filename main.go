f64f826ef18a807b9d08883d96f8b6920491a7a6
inksha <x1a010@163.com>
2026-03-09 15:01:10 +0800

    feat: add verify hash feat

--- main.go
+++ main.go
@@ -22,48 +22,78 @@ func getContent(isFile bool, value string) (io.Reader, error) {
 	return strings.NewReader(value), nil
 }
 
+type NotSupportHashMode struct {
+	Message string
+}
+
+func (e *NotSupportHashMode) Error() string {
+	return e.Message
+}
+
+func computedHash(content io.Reader, mode string) (string, error) {
+	var hashMode hash.Hash
+
+	switch mode {
+	case "md5":
+		hashMode = md5.New()
+	case "sha1":
+		hashMode = sha1.New()
+	case "sha256":
+		hashMode = sha256.New()
+	case "sha512":
+		hashMode = sha512.New()
+	default:
+		return "", &NotSupportHashMode{Message: fmt.Sprintf(`"%s" Is Invalid hash mode`, mode)}
+	}
+
+	io.Copy(hashMode, content)
+
+	hashContent := fmt.Sprintf("%x", hashMode.Sum(nil))
+
+	return hashContent, nil
+}
+
 func registerCommand() *command.SCommand {
 	f := command.NewCommandFlag[bool]("file").ChangeDescription("The file to hash").ChangeValue(false)
 	m := command.NewCommandFlag[string]("mode").ChangeDescription("The hash method").ChangeValue("sha256")
 	c := command.NewCommandFlag[bool]("copy").ChangeDescription("Copy hash content to clipboard").ChangeValue(false)
+	v := command.NewCommandFlag[string]("verify").ChangeDescription("verify hash is equal")
 
-	v := command.NewCommandArg[string]("value").ChangeDescription("Compute hash content")
+	value := command.NewCommandArg[string]("value").ChangeDescription("Compute hash content")
 
 	cmd := command.
 		NewCommand("hash").
 		ChangeDescription("Compute hash content").
-		AddArgs(v).
-		AddFlags(f, m, c).
+		AddArgs(value).
+		AddFlags(f, m, c, v).
 		RegisterHandler(func(cmd *command.SCommand) {
-			content, err := getContent(f.Value, v.Value)
+			content, err := getContent(f.Value, value.Value)
 
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
 
-			var mode hash.Hash
+			hashContent, err := computedHash(content, m.Value)
 
-			switch m.Value {
-			case "md5":
-				mode = md5.New()
-			case "sha1":
-				mode = sha1.New()
-			case "sha256":
-				mode = sha256.New()
-			case "sha512":
-				mode = sha512.New()
-			default:
-				fmt.Printf(`"%s" Is Invalid hash mode`, m.Value)
+			if err != nil {
+				fmt.Println(err)
 				os.Exit(1)
 			}
 
-			io.Copy(mode, content)
-			hashContent := fmt.Sprintf("%x", mode.Sum(nil))
-			fmt.Println(hashContent)
+			if v.Value != "" {
+				if hashContent != v.Value {
+					fmt.Println("Hash content not equal verify content")
+					os.Exit(1)
+				} else {
+					fmt.Println("Hash content equal verify content")
+				}
+			} else {
+				fmt.Println(hashContent)
 
-			if c.Value {
-				clipboard.WriteAll(hashContent)
+				if c.Value {
+					clipboard.WriteAll(hashContent)
+				}
 			}
 		})
 
